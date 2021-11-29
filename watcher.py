ea3ddc47475546da6cededd96ff965fedb091b98
JDRIVO <41508421+JDRIVO@users.noreply.github.com>
2021-11-29 22:42:27 +1100

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -7,7 +7,7 @@ from sqlite3 import dbapi2 as sqlite
 
 def run():
 	monitor = xbmc.Monitor()
-	watcher = LibraryWatch()
+	watcher = LibraryMonitor()
 
 	while not monitor.abortRequested() and watcher.enabled:
 
@@ -15,177 +15,159 @@ def run():
 			break
 
 
-class LibraryWatch(xbmc.Monitor):
+class LibraryMonitor(xbmc.Monitor):
 
 	def __init__(self):
 		self.settings = constants.addon
 		self.getSettings()
 
-	def onNotification(self, sender, method, data):
-
-		if method == "VideoLibrary.OnUpdate":
-			response = json.loads(data)
-
-			if "item" in response and "type" in response.get("item") and response.get("item").get("type") in ("episode", "movie"):
-				dbID = response["item"]["id"]
-				dbType = response["item"]["type"]
-
-				if dbType == "movie":
-					query =	{
-						"jsonrpc": "2.0",
-						"id": "1",
-						"method": "VideoLibrary.GetMovieDetails",
-						"params": {"movieid": dbID, "properties": ["file"]},
-					}
-					jsonKey = "moviedetails"
-				else:
-					query = {
-						"jsonrpc": "2.0",
-						"id": "1",
-						"method": "VideoLibrary.GetEpisodeDetails",
-						"params": {"episodeid": dbID, "properties": ["file"]},
-					}
-					jsonKey = "episodedetails"
-
-				jsonResponse = self.jsonQuery(query)
-
-				strmPath = jsonResponse["result"][jsonKey]["file"]
-				strmName = os.path.basename(strmPath)
-				strmDir = os.path.dirname(strmPath) + os.sep
-				strmData = self.openFile(strmPath)
-
-				mediaDetails = self.mediaDetailConversion(strmData)
-
-				if not mediaDetails:
-					return
-
-				try:
-					databaseQuery = self.databaseAction(
-						"select",
-						(
-							"SELECT idFile FROM files WHERE idPath=(SELECT idPath FROM path WHERE strPath=?) AND strFilename=?",
-							(strmDir, strmName),
-						),
-					)
-				except:
-					xbmc.log(
-						self.settings.getLocalizedString(30003) + ": " + self.settings.getLocalizedString(30221),
-						xbmc.LOGERROR,
-					)
-					return
-
-				fileId = databaseQuery[0][0]
-				insertParams = []
-
-				for k, v in mediaDetails.items():
-					v[0].append("idFile")
-					v[1].append(str(fileId))
-					reconstruct = "".join(
-						[
-							"{}='{}' AND ".format(value, v[1][count])
-							if value != v[0][-1]
-							else "{}='{}'".format(value, v[1][count])
-							for count, value in enumerate(v[0])
-						]
-					)
-					rows, values = ", ".join(v[0]), str(v[1])[1:-1]
-					insertParams.append(
-						"INSERT INTO streamdetails ({}) SELECT {} WHERE NOT EXISTS (SELECT 1 FROM streamdetails WHERE {})".format(
-							rows, values, reconstruct
-						)
-					)
-
-				self.databaseAction("insert", insertParams)
-
-	def openFile(self, path):
+	@staticmethod
+	def openFile(path):
 
 		with open(path, "r") as strm:
 			return strm.read()
 
-	def mediaDetailConversion(self, strmData):
-		# "Apollo 13 (1995) Anniversary Edition&aspect_ratio=33&audio_codec=69"
-		splitText = strmData.split("&")
+	@staticmethod
+	def jsonQuery(query):
+		query = json.dumps(query)
+		return json.loads(xbmc.executeJSONRPC(query))
 
-		videoCodes = {
+	def onNotification(self, sender, method, data):
+
+		if method != "VideoLibrary.OnUpdate":
+			return
+
+		data = json.loads(data)
+
+		if "item" in data and "type" in data.get("item") and data.get("item").get("type") in ("episode", "movie"):
+			dbID = data["item"]["id"]
+			dbType = data["item"]["type"]
+
+			if dbType == "movie":
+				query =	{
+					"jsonrpc": "2.0",
+					"id": "1",
+					"method": "VideoLibrary.GetMovieDetails",
+					"params": {"movieid": dbID, "properties": ["file"]},
+				}
+				jsonKey = "moviedetails"
+			else:
+				query = {
+					"jsonrpc": "2.0",
+					"id": "1",
+					"method": "VideoLibrary.GetEpisodeDetails",
+					"params": {"episodeid": dbID, "properties": ["file"]},
+				}
+				jsonKey = "episodedetails"
+
+			jsonResponse = self.jsonQuery(query)
+
+			strmPath = jsonResponse["result"][jsonKey]["file"]
+			strmName = os.path.basename(strmPath)
+			strmDir = os.path.dirname(strmPath) + os.sep
+			strmData = self.openFile(strmPath)
+
+			mediaInfo = self.mediaInfoConversion(strmData)
+
+			if not mediaInfo:
+				return
+
+			try:
+				fileID = self.select(
+					(
+						"SELECT idFile FROM files WHERE idPath=(SELECT idPath FROM path WHERE strPath=?) AND strFilename=?",
+						(strmDir, strmName),
+					),
+				)
+			except:
+				xbmc.log(
+					self.settings.getLocalizedString(30003) + ": " + self.settings.getLocalizedString(30221),
+					xbmc.LOGERROR,
+				)
+				return
+
+			self.insert(self.statementConstructor(mediaInfo, fileID))
+
+	@staticmethod
+	def mediaInfoConversion(strmData):
+		videoInfo = {
 			"video_codec": "strVideoCodec",
 			"aspect_ratio": "fVideoAspect",
 			"video_width": "iVideoWidth",
 			"video_height": "iVideoHeight",
-			"video_duration": "iVideoDuration"
+			"video_duration": "iVideoDuration",
 		}
-		audioCodes = {
+		audioInfo = {
 			"audio_codec": "strAudioCodec",
-			"audio_channels": "iAudioChannels"
+			"audio_channels": "iAudioChannels",
 		}
+		videoNames, videoValues, audioNames, audioValues = [], [], [], []
+		strmData = strmData.split("&")
 
-		videoRows, videoValues, audioRows, audioValues = [], [], [], []
+		for params in strmData:
+			name, value = params.split("=")
 
-		for mediaDetail in splitText:
-			mediaSplit = mediaDetail.split("=")
-			mediaDetail = mediaSplit[0]
-			match = False
+			if not value:
+				continue
 
-			if mediaDetail in videoCodes:
-				match = True
-				rows = videoRows
-				values = videoValues
-				codes = videoCodes
-			elif mediaDetail in audioCodes:
-				match = True
-				rows = audioRows
-				values = audioValues
-				codes = audioCodes
+			if name in videoInfo:
+				videoNames.append(videoInfo[name])
+				videoValues.append(value)
+			elif name in audioInfo:
+				audioNames.append(audioInfo[name])
+				audioValues.append(value)
 
-			if match:
-				value = mediaSplit[1]
+		converted = []
 
-				if value:
-					rowTitle = codes[mediaDetail]
-					rows.append(rowTitle)
-					values.append(value)
-
-		converted = {}
-
-		if videoRows:
-			videoRows.append("iStreamType")
+		if videoNames:
+			videoNames.append("iStreamType")
 			videoValues.append("0")
-			converted["video"] = [videoRows, videoValues]
+			converted.append((videoNames, videoValues))
 
-		if audioRows:
-			audioRows.append("iStreamType")
+		if audioNames:
+			audioNames.append("iStreamType")
 			audioValues.append("1")
-			converted["audio"] = [audioRows, audioValues]
+			converted.append((audioNames, audioValues))
 
 		if converted:
 			return converted
 
-	def jsonQuery(self, query):
-		query = json.dumps(query)
-		result = xbmc.executeJSONRPC(query)
-		return json.loads(result)
+	@staticmethod
+	def statementConstructor(mediaInfo, fileID):
+		statements = []
 
-	def databaseAction(self, action, arg):
+		for names, values in mediaInfo:
+			names.append("idFile")
+			values.append(fileID)
+			reconstruct = "".join(
+				[
+					"{}='{}' AND ".format(name, values[count])
+					if name != names[-1]
+					else "{}='{}'".format(name, values[count])
+					for count, name in enumerate(names)
+				]
+			)
+			names = ", ".join(names)
+			values = str(values)[1:-1]
+			statements.append(
+				"INSERT INTO streamdetails ({}) SELECT {} WHERE NOT EXISTS (SELECT 1 FROM streamdetails WHERE {})".format(
+					names, values, reconstruct
+				)
+			)
+
+		return statements
+
+	def select(self, statement):
 		db = sqlite.connect(self.dbPath)
+		query = list(db.execute(*statement))
+		db.close()
+		return query[0][0]
 
-		if action == "select":
-			cursor = db.execute(arg[0], arg[1])
-			result = list(cursor)
-			db.close()
-			return list(result)
-
-		elif action == "insert":
-			# strVideoCodec, iVideoWidth, iVideoHeight, fVideoAspect, iStreamType, idFile
-			# 'h264', '1920', '1080', '1.77777777778', '0', '70'
-			# strVideoCodec='h264' AND iVideoWidth='1920' AND iVideoHeight='1080' AND fVideoAspect='1.77777777778' AND iStreamType='0' AND idFile='70'
-
-			# cursor = db.execute("INSERT INTO streamdetails (strVideoCodec, strAudioCodec) SELECT 'test1', 'test2' EXCEPT SELECT strVideoCodec, strAudioCodec FROM streamdetails WHERE strVideoCodec='test1' AND strAudioCodec='test2'")
-			# cursor = db.execute("INSERT INTO streamdetails (strVideoCodec, strAudioCodec) SELECT 'test1', 'test2' WHERE NOT EXISTS (SELECT strVideoCodec, strAudioCodec FROM streamdetails WHERE strVideoCodec='test1' AND strAudioCodec='test2')")
-
-			for v in arg:
-				db.execute(v)
-
-			db.commit()
-			db.close()
+	def insert(self, statements):
+		db = sqlite.connect(self.dbPath)
+		[db.execute(statement) for statement in statements]
+		db.commit()
+		db.close()
 
 	def onSettingsChanged(self):
 		self.getSettings()
@@ -193,3 +175,9 @@ class LibraryWatch(xbmc.Monitor):
 	def getSettings(self):
 		self.enabled = self.settings.getSetting("watcher")
 		self.dbPath = xbmc.translatePath(self.settings.getSetting("video_db"))
+
+	# strVideoCodec, iVideoWidth, iVideoHeight, fVideoAspect, iStreamType, idFile
+	# 'h264', '1920', '1080', '1.77777777778', '0', '70'
+	# strVideoCodec='h264' AND iVideoWidth='1920' AND iVideoHeight='1080' AND fVideoAspect='1.77777777778' AND iStreamType='0' AND idFile='70'
+	# cursor = db.execute("INSERT INTO streamdetails (strVideoCodec, strAudioCodec) SELECT 'test1', 'test2' EXCEPT SELECT strVideoCodec, strAudioCodec FROM streamdetails WHERE strVideoCodec='test1' AND strAudioCodec='test2'")
+	# cursor = db.execute("INSERT INTO streamdetails (strVideoCodec, strAudioCodec) SELECT 'test1', 'test2' WHERE NOT EXISTS (SELECT strVideoCodec, strAudioCodec FROM streamdetails WHERE strVideoCodec='test1' AND strAudioCodec='test2')")
